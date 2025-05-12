2f67de89cac7e8b39758cae707933cc2f4dd615f
Pradeepkumar1407 <pradeephamray@gmail.com>
2025-05-12 22:55:02 +0530

    Upadted index.js for second time

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Updated Docker Container Running Successfully n');
+  res.end('Updated Docker Container Running Successfully for git hub actions n');
 });
 
 server.listen(port, () => {
